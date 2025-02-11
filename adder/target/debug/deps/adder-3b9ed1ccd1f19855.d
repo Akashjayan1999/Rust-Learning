eef033f718f6e17b0c626b98cded10c0c6e93397
Akash Jayan <akash.jayan@sequantix.com>
2025-02-11 20:24:39 +0530

    Remove unused test artifacts and update Rust compiler information

---
+++ adder/target/debug/deps/adder-3b9ed1ccd1f19855.d
@@ -0,0 +1,5 @@
+D:\OneDrive - Sequantix Pvt. Ltd\Learning\Rust_learning\adder\target\debug\deps\libadder-3b9ed1ccd1f19855.rmeta: src\lib.rs
+
+D:\OneDrive - Sequantix Pvt. Ltd\Learning\Rust_learning\adder\target\debug\deps\adder-3b9ed1ccd1f19855.d: src\lib.rs
+
+src\lib.rs:
---
+++ adder/target/debug/deps/adder-cb3253e172fafc0a.d
@@ -0,0 +1,5 @@
+D:\OneDrive - Sequantix Pvt. Ltd\Learning\Rust_learning\adder\target\debug\deps\libadder-cb3253e172fafc0a.rmeta: src\lib.rs
+
+D:\OneDrive - Sequantix Pvt. Ltd\Learning\Rust_learning\adder\target\debug\deps\adder-cb3253e172fafc0a.d: src\lib.rs
+
+src\lib.rs:
